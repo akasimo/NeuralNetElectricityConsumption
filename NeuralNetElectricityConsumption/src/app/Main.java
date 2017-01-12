e74fe13aa2856b32f90ca8959b2297b20d3da899
ggoral111 <ggoral111@gmail.com>
2017-01-12 21:55:23 +0100

    Added another prediction method.

--- NeuralNetElectricityConsumption/src/app/Main.java
+++ NeuralNetElectricityConsumption/src/app/Main.java
@@ -17,11 +17,12 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		predictElectricityConsumption1("2017-01-16", 2015, 5);
-		// predictElectricityConsumption2("2017-01-16", 2015, 5);
+		// predictElectricityConsumption1("2017-01-16", 2015, 5, 12);
+		// predictElectricityConsumption2("2017-01-16", 2015, 5, 12);
+		predictElectricityConsumption3("2017-01-16", 2015, 5, 12);
 	}
 	
-	public static void predictElectricityConsumption1(String date, Integer weatherStartYear, Integer numberOfYears)
+	public static void predictElectricityConsumption1(String date, Integer weatherStartYear, Integer numberOfYears, Integer hourOfDay)
 	{
 		/*********************************************LOADING DATA***********************************************************/
 		
@@ -87,6 +88,8 @@ public class Main {
 			System.out.println();
 		}
 		
+		/***********************************************PRINT WH DATA*********************************************************/
+		
 		Integer avgWeatherYear = weatherStartYear;
 		System.out.println("\nAverage weather history data:\n");
 		
@@ -131,8 +134,7 @@ public class Main {
 		/*************************************************PREPARE DATA*********************************************************/
 		
 		List<Object[]> normalizedInputHourlyWeatherHistory = new ArrayList<Object[]>();
-		Object[] normalizedInputHourlyWeatherForecast = null;		
-		Integer hourOfDay = 12;		// [1;24]
+		Object[] normalizedInputHourlyWeatherForecast = null;
 		
 		for(int j = 0; j < normalizedInputWeather.get(0).get(0).length; j++)
 		{
@@ -160,6 +162,8 @@ public class Main {
 		{
 			nonNormalizedinputHourlyElectricityConsumption[j] = dailyECHistoryAllYears.get(j)[hourOfDay -1];
 		}
+		
+		/**********************************************PRINT PREPARED DATA*****************************************************/
 		
 		System.out.println("\nPrepared hourly weather history data & electricity consumption:\n");
 		
@@ -201,7 +205,7 @@ public class Main {
 				nns.backpropagate(new TrainingData(normalizedInputHourlyWeatherHistory.get(k), new Object[]{normalizedInputHourlyElectricityConsumption[k]}), 2, 3, false);
 			}
 			
-			System.out.print("\nResult of backropagation: ");
+			System.out.print("\nResult of backpropagation: ");
 			
 			for(Object o : nns.propagate(new TrainingData(normalizedInputHourlyWeatherForecast), 2))
 			{
@@ -214,17 +218,10 @@ public class Main {
 			System.out.println("Enlapsed Time: " + timer.enlapsedTime()/1000.00 + "s");
 		}
 		
-		Object averagePredictionResult = 0.;
-		
-		for(Object o : predictionResultList)
-		{
-			averagePredictionResult = nns.getOcs().addObjects(averagePredictionResult, o);
-		}
-		
-		System.out.println("\n\nAverage electricity consumption result: " + nns.getOcs().divideObjects(averagePredictionResult, predictionResultList.size()) + " MWh");
+		System.out.println("\n\nAverage electricity consumption result: " + evaluateAveragePredictionValue(predictionResultList) + " MWh");
 	}
 	
-	public static void predictElectricityConsumption2(String date, Integer weatherStartYear, Integer numberOfYears)
+	public static void predictElectricityConsumption2(String date, Integer weatherStartYear, Integer numberOfYears, Integer hourOfDay)
 	{
 		/*********************************************LOADING DATA***********************************************************/
 		
@@ -293,6 +290,8 @@ public class Main {
 			System.out.println();
 		}
 		
+		/***********************************************PRINT WH DATA*********************************************************/
+		
 		Integer avgWeatherYear = weatherStartYear;
 		System.out.println("\nAverage weather history data:\n");
 		
@@ -338,7 +337,6 @@ public class Main {
 		
 		List<Object[]> normalizedInputHourlyWeatherHistory = new ArrayList<Object[]>();
 		Object[] normalizedInputHourlyWeatherForecast = new Object[normalizedInputWeatherTest.size()];		
-		Integer hourOfDay = 12;		// [1;24]
 		
 		for(int j = 0; j < normalizedInputWeather.get(0).get(0).length; j++)
 		{
@@ -364,6 +362,8 @@ public class Main {
 		{
 			nonNormalizedinputHourlyElectricityConsumption[j] = dailyECHistoryAllYears.get(j)[hourOfDay -1];
 		}
+		
+		/**********************************************PRINT PREPARED DATA*****************************************************/
 		
 		System.out.println("\nPrepared hourly weather history data & electricity consumption:\n");
 		
@@ -405,7 +405,7 @@ public class Main {
 				nns.backpropagate(new TrainingData(normalizedInputHourlyWeatherHistory.get(k), new Object[]{normalizedInputHourlyElectricityConsumption[k]}), 2, 3, false);
 			}
 			
-			System.out.print("\nResult of backropagation: ");
+			System.out.print("\nResult of backpropagation: ");
 			
 			for(Object o : nns.propagate(new TrainingData(normalizedInputHourlyWeatherForecast), 2))
 			{
@@ -418,6 +418,262 @@ public class Main {
 			System.out.println("Enlapsed Time: " + timer.enlapsedTime()/1000.00 + "s");
 		}
 		
+		System.out.println("\n\nAverage electricity consumption result: " + evaluateAveragePredictionValue(predictionResultList) + " MWh");
+	}
+	
+	public static void predictElectricityConsumption3(String date, Integer weatherStartYear, Integer numberOfYears, Integer hourOfDay)
+	{
+		/*********************************************LOADING DATA***********************************************************/
+		
+		DataLoaderService dls = new DataLoaderService();
+		ArrayList<ArrayList<List<Object[]>>> allCitiesDailyWeatherHistoryAllYearsBC = dls.loadWeatherHistoryData(weatherStartYear, numberOfYears, date, new String[]{"Baltimore", "Columbia", "SevernaPark"});
+		ArrayList<ArrayList<List<Object[]>>> allCitiesDailyWeatherHistoryAllYearsPEP = dls.loadWeatherHistoryData(weatherStartYear, numberOfYears, date, new String[]{"Clinton", "Rockville", "Waszyngton"});
+		
+		String[] weatherForecastLink = new String[]{"https://api-ak-aws.wunderground.com/api/c991975b7f4186c0/forecast10day/hourly10day/lang:EN/units:metric/v:2.0/bestfct:1/q/zmw:21201.1.99999.json", // Baltimore
+													"https://api-ak-aws.wunderground.com/api/c991975b7f4186c0/forecast10day/hourly10day/lang:EN/units:metric/v:2.0/bestfct:1/q/zmw:21044.1.99999.json", // Columbia
+													"https://api-ak-aws.wunderground.com/api/c991975b7f4186c0/forecast10day/hourly10day/lang:EN/units:metric/v:2.0/bestfct:1/q/zmw:21146.1.99999.json" //Severna Park
+													};
+		
+		ArrayList<List<Object[]>> allCitiesDailyWeatherForecast = dls.loadWeatherForecastData(weatherForecastLink, new String[]{"Baltimore", "Columbia", "SevernaPark"}, date, new String[]{"WeatherForecast/Baltimore", "WeatherForecast/Columbia", "WeatherForecast/SevernaPark"});
+		List<Object[]> dailyECHistoryAllYearsBC = dls.loadElectricityConsumptionData(weatherStartYear, numberOfYears, date, "BC");
+		List<Object[]> dailyECHistoryAllYearsPEP = dls.loadElectricityConsumptionData(weatherStartYear, numberOfYears, date, "PEP");
+		
+		/*********************************************COMPUTE DATA AVERAGE****************************************************/
+		
+		List<Object[]> avgDailyWeatherForecast = dls.avgForWeatherForecastData(allCitiesDailyWeatherForecast);
+		ArrayList<List<Object[]>> avgForWeatherHistoryDataBC = dls.avgForWeatherHistoryData(allCitiesDailyWeatherHistoryAllYearsBC);
+		ArrayList<List<Object[]>> avgForWeatherHistoryDataPEP = dls.avgForWeatherHistoryData(allCitiesDailyWeatherHistoryAllYearsPEP);
+		
+		/*********************************************NORMALIZE DATA**********************************************************/
+		
+		ArrayList<List<Object[]>> avgForWeatherHistoryDataUnion = avgForWeatherHistoryDataBC;
+		avgForWeatherHistoryDataUnion.addAll(avgForWeatherHistoryDataPEP);
+		
+		ArrayList<List<Object[]>> normalizedInputWeather = dls.normalizeInputForWeatherHistoryWithWeatherForecastData(avgForWeatherHistoryDataUnion, avgDailyWeatherForecast, 1);
+		List<Object[]> normalizedInputElectricityConsumption = dls.normalizeInputForElectricityConsumptionData(dailyECHistoryAllYearsBC, 1);
+		
+		/***********************************************PRINT EC DATA*********************************************************/
+		
+		System.out.println("\nElectricity consumption data:\n");
+		
+		for(int i = 0; i < dailyECHistoryAllYearsBC.get(0).length; i++)
+		{
+			for(int j = 0; j < dailyECHistoryAllYearsBC.size(); j++)
+			{
+				System.out.print(dailyECHistoryAllYearsBC.get(j)[i] + ", ");
+			}
+			
+			System.out.println();
+		}
+		
+		// Electricity - 24 (24 hours) size List with 5 objects (5 years).
+		System.out.println("\nNormalized electricity consumption data:\n");
+		
+		for(Object[] oa : normalizedInputElectricityConsumption)
+		{
+			for(Object o : oa)
+			{
+				System.out.print(o + ", ");
+			}
+			
+			System.out.println();
+		}
+		
+		/***********************************************PRINT WF DATA*********************************************************/
+		
+		System.out.println("\nAverage weather forecast data:\n");
+		
+		for(Object[] oa : avgDailyWeatherForecast)
+		{
+			for(Object o : oa)
+			{
+				System.out.print(o + ", ");
+			}
+			
+			System.out.println();
+		}
+		
+		/***********************************************PRINT WH DATA*********************************************************/
+		
+		Integer avgWeatherYear = weatherStartYear;
+		System.out.println("\nAverage weather history data:\n");
+		
+		for(List<Object[]> loa : avgForWeatherHistoryDataBC)
+		{
+			System.out.println("Year: " + avgWeatherYear + "\n");
+			avgWeatherYear--;
+			
+			for(Object[] oa : loa)
+			{
+				for(Object o : oa)
+				{
+					System.out.print(o + ", ");
+				}
+				
+				System.out.println();
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println("\nNormalized weather history data:\n");
+		
+		for(int i = 0; i < normalizedInputWeather.get(0).size(); i++)
+		{
+			System.out.println("Hour: " + (i + 1));
+			
+			for(int j = 0; j < normalizedInputWeather.get(0).get(0).length; j++)
+			{
+				
+				for(int k = 0; k < normalizedInputWeather.size(); k++)
+				{
+					System.out.print(normalizedInputWeather.get(k).get(i)[j] + ", ");
+				}
+				
+				System.out.println();
+			}
+			
+			System.out.println();			
+		}
+		
+		/*************************************************PREPARE DATA*********************************************************/
+		
+		List<Object[]> normalizedInputHourlyWeatherHistoryBC = new ArrayList<Object[]>();
+		List<Object[]> normalizedInputHourlyWeatherHistoryPEP = new ArrayList<Object[]>();
+		Object[] normalizedInputHourlyWeatherForecast = null;
+		
+		for(int j = 0; j < normalizedInputWeather.get(0).get(0).length; j++)
+		{
+			Object[] normalizedInputHourlyWeatherHistoryArray = new Object[normalizedInputWeather.size()];
+			
+			for(int k = 0; k < normalizedInputWeather.size(); k++)
+			{
+				normalizedInputHourlyWeatherHistoryArray[k] = normalizedInputWeather.get(k).get(hourOfDay - 1)[j];
+			}
+			
+			if(j == normalizedInputWeather.get(0).get(0).length - 1)
+			{
+				normalizedInputHourlyWeatherForecast = normalizedInputHourlyWeatherHistoryArray;
+			}
+			else if(j >= ((normalizedInputWeather.get(0).get(0).length - 1) / 2))
+			{
+				normalizedInputHourlyWeatherHistoryPEP.add(normalizedInputHourlyWeatherHistoryArray);
+			}
+			else
+			{
+				normalizedInputHourlyWeatherHistoryBC.add(normalizedInputHourlyWeatherHistoryArray);
+			}
+		}
+			
+		Object[] normalizedInputHourlyElectricityConsumption = normalizedInputElectricityConsumption.get(hourOfDay - 1);
+		Object[] nonNormalizedinputHourlyElectricityConsumption = new Object[dailyECHistoryAllYearsBC.size()];
+		
+		for(int j = 0; j < dailyECHistoryAllYearsBC.size(); j++)
+		{
+			nonNormalizedinputHourlyElectricityConsumption[j] = dailyECHistoryAllYearsBC.get(j)[hourOfDay -1];
+		}
+		
+		/**********************************************PRINT PREPARED DATA*****************************************************/
+		
+		System.out.println("\nPrepared hourly weather history data & electricity consumption:\n");
+		
+		for(int i = 0; i < normalizedInputHourlyWeatherHistoryBC.size(); i++)
+		{
+			for(int j = 0; j < normalizedInputHourlyWeatherHistoryBC.get(0).length; j++)
+			{
+				System.out.printf("%-25s", normalizedInputHourlyWeatherHistoryBC.get(i)[j]);
+			}
+			
+			System.out.printf("%-25s", normalizedInputHourlyElectricityConsumption[i]);
+			System.out.printf("%-25s\n", nonNormalizedinputHourlyElectricityConsumption[i]);
+		}
+		
+		System.out.println("\nPrepared hourly weather forecast data:\n");
+		
+		for(Object o : normalizedInputHourlyWeatherForecast)
+		{
+			System.out.printf("%-25s", o);
+		}
+		/*********************************************CALCULATE PREDICTION*****************************************************/
+		
+		System.out.println("\n\n------------------------------------------------------------------------------------------------\n");
+		System.out.println("Calculating ../..");
+		
+		ArrayList<List<Object>> predictionPEPResultList = new ArrayList<List<Object>>();
+		List<Object> predictionPEPAverageResultList = new ArrayList<Object>();
+		List<Object> predictionResultList = new ArrayList<Object>();
+		Object predicitionResult = null;
+		Timer timer = new Timer();
+					
+		NeuralNetworkService nns = new NeuralNetworkService(5,1);
+	  	nns.appendLayer(5);
+		
+		for(int k = 0; k < normalizedInputHourlyWeatherHistoryBC.size(); k++)
+		{			
+			List<Object> predictionPEPResultTempList = new ArrayList<Object>();
+		  	timer.startMeasure();
+		  							
+			for(int i = 0; i < 5000; i++)
+			{
+				nns.backpropagate(new TrainingData(normalizedInputHourlyWeatherHistoryBC.get(k), new Object[]{normalizedInputHourlyElectricityConsumption[k]}), 2, 3, false);
+				// nns.backpropagate(new TrainingData(normalizedInputHourlyWeatherHistoryBC.get(k), new Object[]{normalizedInputHourlyElectricityConsumption[k]}), 2, 8, false);
+			}
+									
+			// Propagate for PEP weather history - calculate prediction for predictionPEPResultList.size() years for each year, compute average and get result
+			for(int l = 0; l < normalizedInputHourlyWeatherHistoryPEP.size(); l++)
+			{			
+				for(Object o : nns.propagate(new TrainingData(normalizedInputHourlyWeatherHistoryPEP.get(l)), 2))
+				{
+					predicitionResult = calculateElectricityConsumption(nonNormalizedinputHourlyElectricityConsumption, o);
+					predictionPEPResultTempList.add(predicitionResult);
+				}				
+			}
+			
+			predictionPEPResultList.add(predictionPEPResultTempList);
+			
+			System.out.print("\nResult of backpropagation: ");
+			
+			// Propagate for weather forecast
+			for(Object o : nns.propagate(new TrainingData(normalizedInputHourlyWeatherForecast), 2))
+			{
+				predicitionResult = calculateElectricityConsumption(nonNormalizedinputHourlyElectricityConsumption, o);
+				System.out.println(o + " ~ " + predicitionResult + " MWh");
+				predictionResultList.add(predicitionResult);
+			}
+			
+			timer.endMeasure();
+			System.out.println("Enlapsed Time: " + timer.enlapsedTime()/1000.00 + "s");
+		}
+		
+		for(int i = 0; i < predictionPEPResultList.get(0).size(); i++)
+		{
+			List<Object> predictionPEPAverageResultTempList = new ArrayList<Object>();
+			
+			for(int j = 0; j < predictionPEPResultList.size(); j++)
+			{
+				predictionPEPAverageResultTempList.add(predictionPEPResultList.get(j).get(i));
+			}
+			
+			predictionPEPAverageResultList.add(evaluateAveragePredictionValue(predictionPEPAverageResultTempList));
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < predictionPEPAverageResultList.size(); i++)
+		{		
+			System.out.print("Year: " + weatherStartYear + ":     ");
+			weatherStartYear--;
+			System.out.printf("HD(MWh): %-25s", dailyECHistoryAllYearsPEP.get(i)[hourOfDay -1]);
+			System.out.printf("PRE(MWh): %-25s", predictionPEPAverageResultList.get(i));
+			System.out.println("PERC(%): " + nns.getOcs().multiplyObjects(nns.getOcs().subtractObjects(nns.getOcs().divideObjects(predictionPEPAverageResultList.get(i), dailyECHistoryAllYearsPEP.get(i)[hourOfDay -1]), 1.), 100.));
+		}
+		
+		System.out.println("\nAverage electricity consumption result: " + evaluateAveragePredictionValue(predictionResultList) + " MWh");
+	}
+	
+	private static Object evaluateAveragePredictionValue(List<Object> predictionResultList)
+	{
+		NeuralNetworkService nns = new NeuralNetworkService();
 		Object averagePredictionResult = 0.;
 		
 		for(Object o : predictionResultList)
@@ -425,7 +681,7 @@ public class Main {
 			averagePredictionResult = nns.getOcs().addObjects(averagePredictionResult, o);
 		}
 		
-		System.out.println("\n\nAverage electricity consumption result: " + nns.getOcs().divideObjects(averagePredictionResult, predictionResultList.size()) + " MWh");
+		return nns.getOcs().divideObjects(averagePredictionResult, predictionResultList.size());
 	}
 	
 	private static Object calculateElectricityConsumption(Object[] nonNormalizedinputHourlyElectricityConsumption, Object result)
